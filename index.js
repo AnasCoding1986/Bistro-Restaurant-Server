6a47fd5d32d122cb69b5a89c1f6564c932a21cb0
AnasCoding1986 <anassust.scw@gmail.com>
2024-05-24 23:21:14 +0600

    m-68.7-finished

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdajqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,8 +31,30 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    // jwt related API
+    app.post('/jwt', async(req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({token});
+    })
+
+    // middlewire
+    const verifyToken = (req,res,next) => {
+      console.log('inside verify token',req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({message:'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        
+      }
+      // next();
+    }
+
     // user
-    app.get('/users', async (req,res)=>{
+    app.get('/users', verifyToken, async (req,res)=>{
       const result = await userCollection.find().toArray();
       res.send(result);
     })

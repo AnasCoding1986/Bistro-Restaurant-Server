44a434e65392b3097ff4ff93803d07ba10efeb3a
AnasCoding1986 <anassust.scw@gmail.com>
2024-05-24 09:28:35 +0600

    users get function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdajqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,9 +26,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDb").collection("user");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+
+    // user
+    app.get('/users', async (req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      // insert email if user does not exist
+      // you can do this by many WebAssembly(1. email unique, 2. upsert, 3.simple checking )
+      const query = {email: user.email};
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message: 'user already exist', insertedId: null})
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
     // menu
     app.get('/menu', async(req,res)=>{
@@ -54,6 +75,13 @@ async function run() {
     app.post('/carts', async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 

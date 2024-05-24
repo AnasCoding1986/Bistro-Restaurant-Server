17d276378450c33e251e7761663e9430976c537a
AnasCoding1986 <anassust.scw@gmail.com>
2024-05-24 22:00:44 +0600

    working with jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -50,6 +51,34 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      try {
+        const result = await userCollection.updateOne(filter, updateDoc);
+        if (result.modifiedCount === 1) {
+          res.status(200).send({ message: 'User updated successfully', modifiedCount: result.modifiedCount });
+        } else {
+          res.status(404).send({ message: 'User not found', modifiedCount: result.modifiedCount });
+        }
+      } catch (error) {
+        res.status(500).send({ message: 'Error updating user', error });
+      }
+    });
+    
 
     // menu
     app.get('/menu', async(req,res)=>{
